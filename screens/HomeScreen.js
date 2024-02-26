81ebbb57843b843b8b455559d87436fbb89088a8
Manish Gond <manish.n.gond@gmail.com>
2024-02-26 17:15:01 +0530

    Changes to HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,25 +1,35 @@
+// HomeScreen.js
+import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+      <Text style={styles.text}>Welcome to our Beautiful App</Text>
+      <Text style={styles.subtitle}>Explore and Enjoy!</Text>
     </View>
   );
 };
-
-export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#F4F4F4", // Light Gray background
   },
 
   text: {
     fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 16
-  }
+    color: "#3498db", // Blue color
+    marginBottom: 8,
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: "#2ecc71", // Green color
+  },
 });
+
+export default HomeScreen;
